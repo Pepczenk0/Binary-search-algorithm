8d0f38877f0aaa65ba94ab807f2e0021bc143516
Pepczenko <93174219+Pepczenk0@users.noreply.github.com>
2021-11-16 14:53:32 -0500

    Update binarysearch.py
    edits for final edit

--- binarysearch.py
+++ binarysearch.py
@@ -3,10 +3,10 @@ def binary_search(lst, number_to_find):
     while True:
         middle = round(len(lst) / 2) 
         
-        # uncomment print statement to prove binary search
-        #print(lst)
+        
+        if len(lst) == 1 and middle != number_to_find:
+            return False
         if lst[middle] == number_to_find:
-            print("Found")
             return True
             break
             
@@ -17,4 +17,4 @@ def binary_search(lst, number_to_find):
             del lst[:middle]
         
 lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-binary_search(lst, 2)
+binary_search(lst, 7)
